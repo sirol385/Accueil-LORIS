118a1725463927517ea0dfb9b627be8d2439f78e
Loris GOUHIER <lorisgouhier@gmail.com>
2025-10-25 15:31:26 +0200

    Add files via upload

--- accueil.js
+++ accueil.js
@@ -1,27 +1,36 @@
-// Fonction pour détecter si nous sommes sur Vercel
 function isVercel() {
     return window.location.hostname.includes('vercel.app');
 }
-
-// Fonction pour mettre à jour les liens
-function updateLinks() {
+function updateResources() {
     if (isVercel()) {
-        // Les URLs Vercel pour chaque projet
         const vercelURLs = {
             'revisions': 'https://revisions-loris.vercel.app/',
             'calculatrice': 'https://calculatrice-loris.vercel.app/',
             'password': 'https://password-loris.vercel.app/'
         };
-
-        // Mettre à jour tous les liens
+        const resourcesURLs = {
+            'revisions': 'https://accueil-loris.vercel.app/revisions.png',
+            'calculatrice': 'https://accueil-loris.vercel.app/calculatrice.png',
+            'password': 'https://accueil-loris.vercel.app/password.png'
+        };
         document.querySelectorAll('.btn').forEach(link => {
             const projectType = link.getAttribute('data-project');
             if (projectType && vercelURLs[projectType]) {
                 link.href = vercelURLs[projectType];
             }
         });
+        document.querySelectorAll('.project-card img').forEach(img => {
+            const altText = img.alt.toLowerCase();
+            if (altText.includes('révisions')) {
+                img.src = resourcesURLs['revisions'];
+            } else if (altText.includes('mot de passe')) {
+                img.src = resourcesURLs['password'];
+            } else if (altText.includes('calculatrice')) {
+                img.src = resourcesURLs['calculatrice'];
+            }
+        });
+        document.querySelector('link[rel="stylesheet"]').href = 'https://accueil-loris.vercel.app/accueil.css';
+        document.querySelector('link[rel="icon"]').href = 'https://accueil-loris.vercel.app/accueil.png';
     }
 }
-
-// Exécuter quand le DOM est chargé
-document.addEventListener('DOMContentLoaded', updateLinks);
+document.addEventListener('DOMContentLoaded', updateResources);
